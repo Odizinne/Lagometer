bb98d13ca25e317441c4753f9344b328e6ae3c4e
Odizinne <flora.aubry@proton.me>
2024-07-01 21:09:08 +0200

    setup.py: easier installation for windows

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from cx_Freeze import setup, Executable
 import sys
+import os
+
+build_dir = "build/Lagometer"
+base = None
+
+if sys.platform == "win32":
+    build_dir = os.path.join(os.getenv('LOCALAPPDATA'), "programs", "Lagometer")
+    base = "Win32GUI"
+
+system_icon = "icons/icon.ico" if sys.platform == "win32" else "icons/icon.png"
 
 build_exe_options = {
     "include_files": ["design.ui", "icons/"],
+    "build_exe": build_dir
 }
-
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
-
-system_icon = None
-if sys.platform == "win32":
-    system_icon = "icons/icon.ico"
-else:
-    system_icon = "icons/icon.png"
 
 executables = [
     Executable("lagometer.py", base=base, icon=system_icon)
